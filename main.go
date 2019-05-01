142dca0438ffa4fefa8135acbfc51f003ee8d241
AaronEdell <aaronedell@gmail.com>
2019-05-01 12:30:30 -0700

    Added main.go

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	pb "gopkg.in/cheggaaa/pb.v1"
+)
+
+func main() {
+	var (
+		dir = flag.String("dir", ".", "source of files to delete")
+		num = flag.Int("num", 123, "number of files to delete")
+	)
+	flag.Parse()
+	files, err := ioutil.ReadDir(*dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	total := len(files)
+	bar := pb.StartNew(*num)
+	totaldeleted := 0
+	for i := 0; i < *num; i++ {
+		rando := randO(1, total)
+		selectedfile := *dir + files[rando].Name()
+		if _, err := os.Stat(selectedfile); err == nil {
+			try := os.Remove(selectedfile)
+			if try != nil {
+				log.Fatal(try)
+			}
+			bar.Increment()
+			totaldeleted++
+		} else if os.IsNotExist(err) {
+			i--
+		} else {
+			log.Fatal(err)
+		}
+	}
+	bar.Finish()
+	log.Printf("Total files deleted: %d", totaldeleted)
+}
+
+func randO(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	randomnumber := rand.Intn(max-min) + min
+	return randomnumber
+}
